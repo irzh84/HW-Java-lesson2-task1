e984d1d96449282e059331aab2720304bbdd9e34
Irina <izhts1482@gmail.com>
2023-08-29 18:09:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 8400;
+        int freemile = 20;
+        int bonus = ticketprice / freemile;
+
+        System.out.println(bonus);
+    }
+}
